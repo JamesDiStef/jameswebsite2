81468253973d10a1bb67e283a0ba6c7a22b9d578
James DiStefano <jamesdistefano@MacBook-Pro.local>
2024-04-17 15:07:03 -0400

    header component starter

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-import logo from "./logo.svg";
+import Header from "./Header";
 import "./App.css";
 
 function App() {
   return (
-    <span className="flex justify-center align-middle">
-      <h1 className="float-bottom text-3xl font-bold underline">Hello Sean!</h1>
-
-      {/* <div class="w-full h-screen bg-gray-200 flex justify-center items-center">
-        <div class="bg-gray-400 w-96 h-96 relative z-0">
-          <p class="italic text-bold bd-red-100 font-serif">Map</p>
-          <div class="absolute inset-0 flex justify-center items-center z-10">
-            <p class="text-2xl font-bold">You suck</p>
-          </div>
-        </div>
-      </div> */}
+    <span>
+      <Header />
+      <h1 className="flex justify-center float-top text-3xl font-bold underline pb-10">
+        Hello Sean!
+      </h1>
+      <br />
     </span>
   );
 }

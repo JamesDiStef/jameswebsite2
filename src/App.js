d4986e86e11e6f6302b3fa18f13787d18fdc0f56
James DiStefano <jamesdistefano@MacBook-Pro.local>
2024-04-17 14:42:10 -0400

    commenting out stuff

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ function App() {
     <span className="flex justify-center align-middle">
       <h1 className="float-bottom text-3xl font-bold underline">Hello Sean!</h1>
 
-      <div class="w-full h-screen bg-gray-200 flex justify-center items-center">
+      {/* <div class="w-full h-screen bg-gray-200 flex justify-center items-center">
         <div class="bg-gray-400 w-96 h-96 relative z-0">
           <p class="italic text-bold bd-red-100 font-serif">Map</p>
           <div class="absolute inset-0 flex justify-center items-center z-10">
             <p class="text-2xl font-bold">You suck</p>
           </div>
         </div>
-      </div>
+      </div> */}
     </span>
   );
 }
